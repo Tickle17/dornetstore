1c3f19317b6ce23bbb2f55570ff0683f97537f22
Vasya <t.lila@list.ru>
2023-05-26 02:58:12 +0500

    feat: improve the nginx starting script

--- run-nginx.sh
+++ run-nginx.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker run -v "$(pwd)/build":/usr/share/nginx/html:ro -v "$(pwd)/nginx.conf":/etc/nginx/nginx.conf:ro -p 80:80 nginx
+readonly CONTAINER_NAME="dornetstore-nginx"
+
+echo "Starting NGINX"
+
+docker rm -f "${CONTAINER_NAME}"
+docker run \
+  --name "${CONTAINER_NAME}" \
+  -v "$(pwd)/build:/usr/share/nginx/html:ro" \
+  -v "$(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro" \
+  -p 80:80 \
+  nginx:1.25.0-alpine
